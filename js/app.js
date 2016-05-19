9ad3561511dae02954877a19e83f56f5b30be72a
Le Phuoc Thang Nguyen <thang@fromttm.com>
2016-05-18 18:02:38 -0700

    1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
 /**
  * Created by ThangTheMan on 5/13/16.
  */
+$(document).ready(function(){
+    $('.hero-image').slick({
+        accessibility:true,
+        autoplay: true,
+        autoplaySpeed:3000,
+        arrows:false,
+        dots:true,
+
+    });
+
+});
+$('.centerList').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows:true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3,
+
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2,
+            }
+        }
+    ]
+});
