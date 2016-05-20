da2774fc5f69d2479040b6dfb293072d9d293cd6
Le Phuoc Thang Nguyen <thang@fromttm.com>
2016-05-19 17:01:15 -0700

    1

--- js/app.js
+++ js/app.js
@@ -2,43 +2,57 @@
  * Created by ThangTheMan on 5/13/16.
  */
 $(document).ready(function(){
-    $('.hero-image').slick({
+
+    $(".banner").slick({
+        centerMode: false,
+        dots:true,
         accessibility:true,
         autoplay: true,
         autoplaySpeed:3000,
-        arrows:false,
-        dots:true,
+        infinite: true,
+        prevArrow: '<a href="@" class="arrows left"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>',
+        nextArrow: '<a href="@" class="arrows right"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>',
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
 
+
+    $(".banner").prepend("<div class='banner-wrapper'></div>");
+    function changebannerbg(){
+        imgpath=($(".banner .slick-active").children("img").attr("src"));
+        $(".banner .banner-wrapper").css({'height':$(".slick-slide").height(),'width':$(".slick-slide").width(),'background-image':'url('+imgpath+')'});
+    }
+    changebannerbg();
+
+    $(".banner .arrows").click(function(){
+        changebannerbg();
+    });
+
+    $('.centerList').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows:true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '50px',
+                    slidesToShow: 2,
+
+                }
+            }
+        ]
     });
 
 });
-$('.centerList').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    arrows:true,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3,
 
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 2,
-            }
-        }
-    ]
-});
+
+
+
+
